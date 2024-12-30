90779e400ba3420de9090ee8dac7443ec53311c3
Jozef Hoschek <xhoschek@fi.muni.cz>
2024-12-30 23:11:00 +0100

    change to module pass instead of function pass

--- llvm/include/llvm/Transforms/Instrumentation/Anaconda.h
+++ llvm/include/llvm/Transforms/Instrumentation/Anaconda.h
@@ -2,12 +2,20 @@
 #define LLVM_TRANSFORMS_UTILS_ANACONDA_H
 
 #include "llvm/IR/PassManager.h"
+#include "llvm/IR/Value.h"
+#include "llvm/IR/GlobalValue.h"
+#include "llvm/IR/ValueMap.h"
+
+#include <map>
 
 namespace llvm {
 
+
+// this pass should be a module runner as it modifies global variables and inserts functions
+//
 class AnacondaPass : public PassInfoMixin<AnacondaPass> {
 public:
-  PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM);
+  PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM);
 
   static bool isRequired() { return true; }
 };
